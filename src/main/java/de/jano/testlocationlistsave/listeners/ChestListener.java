e2c617cd4c8ceab7fab92fa25fa98473d60997a1
JanoDasHuhn <90400307+JanoDasHuhn@users.noreply.github.com>
2022-12-27 23:30:48 +0100

    removed some messages

--- src/main/java/de/jano/testlocationlistsave/listeners/ChestListener.java
+++ src/main/java/de/jano/testlocationlistsave/listeners/ChestListener.java
@@ -1,27 +1,26 @@
 package de.jano.testlocationlistsave.listeners;
 
 import de.jano.testlocationlistsave.managers.LocationsManager;
-import org.bukkit.Bukkit;
+
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 
 public class ChestListener implements Listener {
-    private LocationsManager locationsManager;
+    private final LocationsManager locationsManager;
     public ChestListener(LocationsManager locationsManager) {
         this.locationsManager = locationsManager;
     }
     @EventHandler
     public void onChestOpen(BlockBreakEvent event) {
-        Bukkit.broadcastMessage("Block broken!");
+
 
         if(!event.getBlock().getType().name().contains("CHEST")) return;
-        Bukkit.broadcastMessage("Chest broken!");
-        Bukkit.broadcastMessage("Locations: " + locationsManager.getLocations().size());
+
         if(!locationsManager.getLocations().contains(event.getBlock().getLocation())) return;
-        Bukkit.broadcastMessage("Chest is in list!");
+
         event.setCancelled(true);
-        Bukkit.broadcastMessage("You can't break this chest!");
+
 
     }
 }
